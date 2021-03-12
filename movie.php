e3d5522fdd1b845a34f11db05f6d5b1792571130
Cristiano <cristiano.tarconi@gmail.com>
2021-03-12 19:18:48 +0100

    method tests

--- movie.php
+++ movie.php
@@ -17,17 +17,42 @@ class Movie {
     public $cast;
     public $descrizione;
     public $genere;
+    public $visione_minori;
 
     // Genero un costruttore della Classe creata
     public function __construct($_titolo)
     {
         $this->titolo = $_titolo;
     }
+
+    // Creazione metodo
+    public function setVisione($_genere)
+    {
+        if($_genere == 'horror') {
+            $this->genere = $_genere;
+            $this->visione_minori = false;
+        }
+    }
+
+    public function getVsione()
+    {
+        return $this->visione_minori;
+    }
 }
 
 $movie1 = new Movie('Mission Impossible');
+
 echo $movie1->titolo;
+
 $movie1->voto = 7.5;
+
 echo '<br>';
+
 echo $movie1->voto;
+
+$movie1->cast = ['Jeremy Renner', 'Simon Pegg', 'Paula Patton', 'Michael Nyqvist', 'Vladimir Mashkov', 'Josh Holloway', 'Anil Kapoor', 'Léa Seydoux', 'Tom Wilkinson'];
+
+$movie1->durata = 128;
+$movie1->setVisione('horror');
+
 var_dump($movie1);
